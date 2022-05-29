917ef416352ca2a867732c5bb376292f29bd8431
Jahnavi Sharma <64760311+januniku812@users.noreply.github.com>
2022-05-29 17:38:39 -0500

    Update split_file.py

--- split_file.py
+++ split_file.py
@@ -4,11 +4,12 @@ def main():
     chunk_size = 4  # lines
 
     def write_chunk(part, lines):
+        # making multiple data part outputs with new numbers at the end to signify what fraction of the file it correlates to
         with open('data_part_'+ str(part) +'.csv', 'w') as f_out:
             f_out.write(header)
             f_out.writelines(lines)
 
-    with open(r'C:\Users\Tanya\Desktop\Sheet1.csv', 'r') as f:
+    with open(r'C:\Users\jshar\Desktop\Sheet1.csv', 'r') as f:
         count = 0
         header = f.readline()
         lines = []
