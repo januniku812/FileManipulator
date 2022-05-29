4c3d7069f9fdcec67ad155926eb69078882fc992
Jahnavi Sharma <64760311+januniku812@users.noreply.github.com>
2022-05-29 17:37:08 -0500

    Create split_file.py

---
+++ split_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def main():
+    chunk_size = 4  # lines
+
+    def write_chunk(part, lines):
+        with open('data_part_'+ str(part) +'.csv', 'w') as f_out:
+            f_out.write(header)
+            f_out.writelines(lines)
+
+    with open(r'C:\Users\Tanya\Desktop\Sheet1.csv', 'r') as f:
+        count = 0
+        header = f.readline()
+        lines = []
+        for line in f:
+            count += 1
+            lines.append(line)
+            if count % chunk_size == 0:
+                write_chunk(count // chunk_size, lines)
+                lines = []
+        # write remainder
+        if len(lines) > 0:
+            write_chunk((count // chunk_size) + 1, lines)
+
+if __name__ == '__main__':
+    main()
